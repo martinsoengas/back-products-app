fb09ce24652c8bf6a13c51ecc918cdaa273a1ad7
Martin Soengas <martinsoengas@Martins-MacBook.local>
2022-08-10 11:09:23 -0300

    setting up user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const routes = express.Router();
+
+const { login } = require("../controllers/userController");
+
+routes.post("/login", login);
+
+module.exports = routes;
